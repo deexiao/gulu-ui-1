d7ab8943edc760d4b712b2d6de7e7bfb6a3c2fd4
deexiao <mopjtv@gmail.com>
2022-08-11 13:03:34 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Author: Dee.Xiao
+ * @Date: 2022-08-11 12:25:31
+ * @LastEditors: Dee.Xiao
+ * @LastEditTime: 2022-08-11 12:57:52
+ * @Description: 
+-->
+<template>
+  <div>hi</div>
+  <DeeXiao />
+</template>
+
+<script>
+import DeeXiao from './components/DeeXiao.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DeeXiao,
+  },
+}
+</script>
---
+++ src/components/DeeXiao.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Author: Dee.Xiao
+ * @Date: 2022-08-11 12:37:06
+ * @LastEditors: Dee.Xiao
+ * @LastEditTime: 2022-08-11 12:56:39
+ * @Description: 
+-->
+<template lang="">
+  <div>deexiao...</div>
+</template>
+<script>
+export default {}
+</script>
+<style lang=""></style>
